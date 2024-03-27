2a27e1593113e0d0a96a22f84fb3914c2e14dfa9
Harshtank-io <tankharsh67@gmail.com>
2024-03-27 17:06:36 +0530

    inputbox size incresed

--- src/component/Authentication/verify.jsx
+++ src/component/Authentication/verify.jsx
@@ -1,17 +1,16 @@
-
 // const Verify = () => {
 //   return (
 //     <div>
 //         <div className="container pt-16">
 //         {/* <!--[--> */}
 //             <div className="flex justify-start">
-//                 <h1 className="font-poppins font-semibold text-black text-4xl">Verify</h1>
+//                 <h1 className="text-4xl font-semibold text-black font-poppins">Verify</h1>
 //             </div>
 //             <div className="mt-2">
-//                 <span className="text-grayB text-wrap font-poppins text-sm font-normal pr-3">Enter the code sent to xxxxx xxxxx</span>
+//                 <span className="pr-3 text-sm font-normal text-grayB text-wrap font-poppins">Enter the code sent to xxxxx xxxxx</span>
 //             </div>
 //             <div className="flex justify-start">
-//                 <input type="number" inputMode="numeric" className="w-20 h-16 mt-8 border border-grayB rounded-lg"/>
+//                 <input type="number" inputMode="numeric" className="w-20 h-16 mt-8 border rounded-lg border-grayB"/>
 //             </div>
 //         </div>
 //     </div>
@@ -19,10 +18,10 @@
 // }
 
 // export default Verify;
-import  { useState, useRef, useEffect } from 'react';
+import { useState, useRef, useEffect } from "react";
 
 function Verify() {
-  const [codes, setCodes] = useState(['', '', '', '']);
+  const [codes, setCodes] = useState(["", "", "", ""]);
   const codeInputs = useRef([]);
 
   useEffect(() => {
@@ -35,53 +34,57 @@ function Verify() {
       const newCodes = [...codes];
       newCodes[index] = value;
       setCodes(newCodes);
-      if (value !== '' && index < 3) {
+      if (value !== "" && index < 3) {
         codeInputs.current[index + 1].focus();
       }
     }
   };
 
   const handleKeyDown = (index, e) => {
-    if (e.key === 'Backspace' && index > 0 && codes[index] === '') {
+    if (e.key === "Backspace" && index > 0 && codes[index] === "") {
       codeInputs.current[index - 1].focus();
     }
   };
 
   return (
     <div>
-        <div className='container pt-16'>
-            <div className='flex justify-start'>
-                <h1 className='text-black font-poppins text-4xl'>Verify</h1>
-            </div>
-            <div className="mt-2 flex justify-start">
-                <p className="text-grayB text-wrap font-poppins text-sm font-normal">Enter the code sent to xxxxx xxxxx</p>
-            </div>
-      <div className="flex grow h-64 mt-8">
-        {codes.map((code, index) => (
-          <input
-            key={index}
-            type="text"
-            id={`verificationCode${index}`}
-            name={`verificationCode${index}`}
-            value={code}
-            onChange={(e) => handleChange(index, e)}
-            onKeyDown={(e) => handleKeyDown(index, e)}
-            ref={(input) => (codeInputs.current[index] = input)}
-            maxLength="1"
-            autoComplete="off"
-            className="w-12 h-12 text-2xl text-center border border-gray-300 rounded-md mr-2 focus:outline-none focus:border-blue-500"
-          />
-        ))}
-           </div>
-            <div className='flex border-2  mt-9 font-thin rounded-xl bg-BrandColor'>
-                <button type="submit" className="font-poppins w-full py-4 px-4 gap-2 text-white font-normal text-sm">Continue</button>
-            </div>
-        
+      <div className="container pt-16">
+        <div className="flex justify-start">
+          <h1 className="text-4xl text-black font-poppins">Verify</h1>
+        </div>
+        <div className="flex justify-start mt-2">
+          <p className="text-sm font-normal text-grayB text-wrap font-poppins">
+            Enter the code sent to xxxxx xxxxx
+          </p>
+        </div>
+        <div className="flex h-64 mt-8 grow">
+          {codes.map((code, index) => (
+            <input
+              key={index}
+              type="text"
+              id={`verificationCode${index}`}
+              name={`verificationCode${index}`}
+              value={code}
+              onChange={(e) => handleChange(index, e)}
+              onKeyDown={(e) => handleKeyDown(index, e)}
+              ref={(input) => (codeInputs.current[index] = input)}
+              maxLength="1"
+              autoComplete="off"
+              className="w-16 h-16 mr-2 text-2xl text-center border border-gray-300 rounded-2xl focus:outline-none focus:border-blue-500"
+            />
+          ))}
+        </div>
+        <div className="flex font-thin border-2 mt-9 rounded-xl bg-BrandColor">
+          <button
+            type="submit"
+            className="w-full gap-2 px-4 py-4 text-sm font-normal text-white font-poppins"
+          >
+            Continue
+          </button>
+        </div>
       </div>
-      
     </div>
   );
 }
 
 export default Verify;
-
